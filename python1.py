76fb2e7f0efff5996dc7ff91f2a7e328992b3ac4
martenczuk <kamil.martenczuk@outlook.com>
2020-02-29 12:00:38 +0100

    ide do domu

---
+++ python1.py
@@ -0,0 +1,35 @@
+print("Hallo World!")
+
+# def main():
+#     for elem in kolekcja:
+#         constam()
+liczba = "Ala"
+liczba = 0
+liczba = 0.0
+# PEP8
+liczba = "Ala"
+liczba = 10
+imie = "Ala"
+print(liczba)
+print(type(liczba))
+print(type(5))
+print(type(4.4))
+print(type(True))
+print(type(None))
+
+imie = str("Ala")
+liczba = str(199)
+
+binarnie = "101"
+
+imie.lower()
+# imie[0] = 'a' blad
+print(imie)
+
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
+# <class 'bool'>
+# <class 'NoneType'>
+
+
